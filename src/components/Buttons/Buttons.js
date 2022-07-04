e059eebb250e76f989a1031b7b452be61a27bf89
Mariana <mariamna5204@gmail.com>
2022-07-04 19:32:21 +0300

    Update Buttons.js

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,12 +1,12 @@
-import styles from './Buttons.module.css';
+import './Buttons.module.css';
 
 const Buttons = () => {
-    <div className={styles.feedback_btn_block}>
-        <p className={styles.title}>Please, leave feedback</p>
-        <ul className={styles.feedback_btn_list}>
-            <li className={styles.feedback_btn}>Good</li>
-            <li className={styles.feedback_btn}>Neutral</li>
-            <li className={styles.feedback_btn}>Bad</li>
+    <div className="feedback_btn_block">
+        <p className="title">Please, leave feedback</p>
+        <ul className="feedback_btn_list">
+            <li className="feedback_btn">Good</li>
+            <li className="feedback_btn">Neutral</li>
+            <li className="feedback_btn">Bad</li>
         </ul>
     </div>
 };
