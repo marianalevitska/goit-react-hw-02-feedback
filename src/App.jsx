473efa81f616b2c0d4ce7cafec58b46baa4c133e
Mariana <mariamna5204@gmail.com>
2022-07-13 21:33:34 +0300

    setState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import Buttons from './components/Buttons/Buttons';
+// import Buttons from './components/Buttons/Buttons';
 import Statistics from './components/Statistics/Statistics';
 import { Component } from 'react';
 
 export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
   render() {
     return (
       <div>
-        <Buttons />
-        <Statistics />
+        <Statistics title='Statistics' />
       </div >
     );
   }
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,20 +1,35 @@
 import { Component } from 'react';
 import stl from './Statistics.module.css';
 
-export class Statistics extends Component {
+class Statistics extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+    }
     render() {
         return (
-            < div className={stl.stat_box} >
-                <h1 className={stl.stat_title}>Statistics</h1>
-                <ul className={stl.stat_list}>
-                    <li className={stl.stat_item}>Good:</li>
-                    <li className={stl.stat_item}>Neutral:</li>
-                    <li className={stl.stat_item}>Bad:</li>
-                </ul>
-            </div >
+            <div className={stl.stat}>
+                <div className={stl.feedback_btn_block}>
+                    <h1 className={stl.title}>Please, leave feedback</h1>
+                    <ul className={stl.feedback_btn_list}>
+                        <li className={stl.feedback_btn}><button type='button' onClick={() => this.setState(state => ({ good: state.good + 1 }))}>Good</button></li>
+                        <li className={stl.feedback_btn}><button type='button' onClick={() => this.setState(state => ({ neutral: state.neutral + 1 }))}>Neutral</button></li>
+                        <li className={stl.feedback_btn}><button type='button' onClick={() => this.setState(state => ({ bad: state.bad + 1 }))}>Bad</button></li>
+                    </ul>
+                </div>
+                <div className={stl.stat_block}>
+                    <h1 className={stl.stat_title}>Statistics</h1>
+                    <ul className={stl.stat_list}>
+                        <li className={stl.stat_item}>Good: {this.state.good}</li>
+                        <li className={stl.stat_item}>Neutral: {this.state.neutral}</li>
+                        <li className={stl.stat_item}>Bad: {this.state.bad}</li>
+                    </ul>
+                </div>
+            </div>
         );
-
     };
 };
+
 
 export default Statistics;
