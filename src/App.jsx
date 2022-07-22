3c4b403822e35f49e84e3a6e4889a2ce8e9c06fc
Mariana <mariamna5204@gmail.com>
2022-07-22 20:47:03 +0300

    changing state in statistic to state and relatives in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import Statistics from './components/Statistics/Statistics';
 import { Component } from 'react';
 
-const options = ['good', 'neutral', 'bad'];
+// const options = ['good', 'neutral', 'bad'];
 
 export class App extends Component {
   state = {
@@ -10,15 +10,24 @@ export class App extends Component {
     neutral: 0,
     bad: 0
   }
-  // PositiveFeedback() {
-  //   console.log(this.state);
-  //   const number = ((this.state.good) / (this.state.good + this.state.neutral + this.state.bad)) * 100;
-  //   return number;
-  // }
+  PositiveFeedback() {
+    console.log(this.state);
+    const number = ((this.state.good) / (this.state.good + this.state.neutral + this.state.bad)) * 100;
+    return number;
+  }
+
+  TotalFeedback() {
+    const n = this.state.good + this.state.neutral + this.state.bad;
+    return n;
+  }
   render() {
+    const percent = this.PositiveFeedback();
+    const total = this.TotalFeedback();
+    const positiveFeedback = percent ? percent : 0;
+    const { good, neutral, bad } = this.state;
     return (
       <div>
-        <Statistics title='Statistics' /*good={ } neutrel={ } total={ } positivePercentage={ }*/ />
+        <Statistics title='Statistics' good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positiveFeedback} />
       </div >
     );
   }
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,46 +1,41 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import PropTypes from 'prop-types';
 import stl from './Statistics.module.css';
 
-class Statistics extends Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    }
-    // PositiveFeedback() {
-    //     console.log(this.state);
-    //     const number = ((this.state.good) / (this.state.good + this.state.neutral + this.state.bad)) * 100;
-    //     return number;
-    // }
+function Statistics({ good, neutral, bad, total, positiveFeedback }) {
 
-    render() {
-        const positiv = this.PositiveFeedback;
-        // console.log(positiv);
-        return (
-            <div className={stl.stat}>
-                <div className={stl.feedback_btn_block}>
-                    <h1 className={stl.title}>Please, leave feedback</h1>
-                    <ul className={stl.feedback_btn_list}>
-                        <li className={stl.feedback_btn}><button type='button' onClick={() => this.setState(state => ({ good: state.good + 1 }))}>Good</button></li>
-                        <li className={stl.feedback_btn}><button type='button' onClick={() => this.setState(state => ({ neutral: state.neutral + 1 }))}>Neutral</button></li>
-                        <li className={stl.feedback_btn}><button type='button' onClick={() => this.setState(state => ({ bad: state.bad + 1 }))}>Bad</button></li>
-                    </ul>
-                </div>
-                <div className={stl.stat_block}>
-                    <h1 className={stl.stat_title}>Statistics</h1>
-                    <ul className={stl.stat_list}>
-                        <li className={stl.stat_item}>Good: {this.state.good}</li>
-                        <li className={stl.stat_item}>Neutral: {this.state.neutral}</li>
-                        <li className={stl.stat_item}>Bad: {this.state.bad}</li>
-                        <li className={stl.stat_item}>Total: {this.state.good + this.state.neutral + this.state.bad}</li>
-                        <li className={stl.stat_item}>Positive feedback: {/*positiv*/ ((this.state.good) / (this.state.good + this.state.neutral + this.state.bad)) * 100}%</li>
-                    </ul>
-                </div>
+    return (
+        <div className={stl.stat}>
+            <div className={stl.feedback_btn_block}>
+                <h1 className={stl.title}>Please, leave feedback</h1>
+                <ul className={stl.feedback_btn_list}>
+                    <li className={stl.feedback_btn}><button type='button' onClick={() => this.setState(state => ({ good: state.good + 1 }))}>Good</button></li>
+                    <li className={stl.feedback_btn}><button type='button' onClick={() => this.setState(state => ({ neutral: state.neutral + 1 }))}>Neutral</button></li>
+                    <li className={stl.feedback_btn}><button type='button' onClick={() => this.setState(state => ({ bad: state.bad + 1 }))}>Bad</button></li>
+                </ul>
             </div>
-        );
-    };
-
+            <div className={stl.stat_block}>
+                <h1 className={stl.stat_title}>Statistics</h1>
+                <ul className={stl.stat_list}>
+                    <li className={stl.stat_item}>Good: {good}</li>
+                    <li className={stl.stat_item}>Neutral: {neutral}</li>
+                    <li className={stl.stat_item}>Bad: {bad}</li>
+                    <li className={stl.stat_item}>Total: {total}</li>
+                    <li className={stl.stat_item}>Positive feedback: {positiveFeedback}%</li>
+                </ul>
+            </div>
+        </div>
+    );
 };
+Statistics.propTypes = {
+    good: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    positiveFeedback: PropTypes.number.isRequired,
+};
+
+
 
 
 export default Statistics;
